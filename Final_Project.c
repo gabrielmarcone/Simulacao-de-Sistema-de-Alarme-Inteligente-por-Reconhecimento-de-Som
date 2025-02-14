6f2db20526bafe9f2279fc95c803c77b503aa3bb
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-02-14 10:33:45 -0300

    init comit

---
+++ Final_Project.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+#include "ws2812.pio.h"
+
+int main() {
+    stdio_init_all();
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
