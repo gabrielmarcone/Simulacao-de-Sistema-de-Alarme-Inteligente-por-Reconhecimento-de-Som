0e33f128fb71bb3094a03c35505adf00b1f0d8db
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-02-17 07:19:40 -0300

    adição das variáveis para acessar os pinos GPIO

--- Final_Project.c
+++ Final_Project.c
@@ -7,6 +7,32 @@
 #include "hardware/pwm.h"
 #include "ws2812.pio.h"
 
+// Definindo os pinos dos LEDs
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
+
+// Variáveis para a matriz de LEDs
+#define MATRIX_PIN 7
+#define NUM_LEDS 25
+
+// Definindo os pinos dos botões
+#define BUTTON_A 5
+#define BUTTON_B 6
+
+// Definindo os pinos dos buzzers
+#define BUZZER_A 10
+#define BUZZER_B 21
+
+// Definindo o pino para o microfone
+#define MIC_PIN 28
+
+// Variáveis para a comunicação I2C
+#define I2C_PORT i2c1 // Porta I2C
+#define I2C_SDA 14 // Pino para SDA
+#define I2C_SCL 15 // Pino para SCL
+#define SSD1306_ADDR 0x3C // Endereço do display OLED
+
 int main() {
     stdio_init_all();
 
